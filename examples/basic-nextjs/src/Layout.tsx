181e18b917a4dfea4579cdb2b0cd94b66b3d1c45
fedmuj <federicomujica1@gmail.com>
2026-02-25 16:06:28 +0100

    Enhance Layout component with additional wrapper for improved structure
    - Updated the Layout component to include a new div wrapper within the main section, enhancing the layout structure and ensuring better alignment with Tailwind CSS styling conventions.

--- examples/basic-nextjs/src/Layout.tsx
+++ examples/basic-nextjs/src/Layout.tsx
@@ -72,7 +72,8 @@ const Layout = ({ page }: LayoutProps): JSX.Element => {
                 )}
               </div>
             </header>
-            <main>
+            <main >
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div id="content">
                 {route && (
                   <AppPlaceholder
@@ -82,6 +83,7 @@ const Layout = ({ page }: LayoutProps): JSX.Element => {
                     rendering={route}
                   />
                 )}
+              </div>
               </div>
             </main>
             <footer>
